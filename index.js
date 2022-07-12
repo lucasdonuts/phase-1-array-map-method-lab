c1bac3a4c197a2427135e8b75f7a542b1535ff41
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-12 15:47:00 -0500

    Done, I think

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (array) => {
+  return array.map(sentence => sentence.split(' ').map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }).join(' '));
 }
